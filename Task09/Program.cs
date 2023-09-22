3dc99c84a26c8369419ff5d979c73fcf7744966d
Yura <yura.tarasevichunt@gmail.com>
2023-09-22 21:41:20 +0300

    Add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая 
+// 1.выводит случайное число из отрезка [10, 99] и 
+// 2. показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+int numberRandom = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка [10,99] => {numberRandom}");
+int remainder = numberRandom % 10;
+int quotient = numberRandom / 10;
+// if (remainder > quotient)
+// {
+//     Console.WriteLine(remainder);
+// }
+// else
+// {
+//     Console.WriteLine(quotient);
+// }
+int maxDigit = quotient > remainder ? quotient : remainder;
+Console.WriteLine(maxDigit);

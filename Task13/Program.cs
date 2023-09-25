a3331e4b148fac9fc8554f9091e6e3e3cbbd53be
Yura <yura.tarasevichunt@gmail.com>
2023-09-25 22:44:15 +0300

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая 
+// 1. выводит третью цифру заданного числа или 
+// 2. сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = Prompt("Введите число");
+if (IsNumberOk(num))
+{
+    Console.WriteLine(ThirdDigit(num));
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+int ThirdDigit(int number)
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    return number % 10;
+}
+
+bool IsNumberOk(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}

f4fc00940c394ba3e8a4d123ba8ec7bb82c676f3
Yura <yura.tarasevichunt@gmail.com>
2023-09-26 00:55:48 +0300

    Add  Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход цифру, обозначающую день недели, и 
+// 2. проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int num = Prompt("Введите число, соответствующее дню недели");
+if (IsDayWeek(num))
+{
+    Weekend(num);
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsDayWeek(int number)
+{
+    if (number < 1 || number > 7)
+    {
+        Console.WriteLine("Ошибка ввода");
+        return false;
+    }
+    return true;
+}
+
+void Weekend(int number)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+}

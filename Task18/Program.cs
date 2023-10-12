4d52e768b3be70a2e8a9816d7b02817ca10b93d7
Yura <yura.tarasevichunt@gmail.com>
2023-10-12 13:32:33 +0300

    Add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, 
+// 1. которая по заданному номеру четверти, 
+// 2. показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    return value;
+}
+
+string rangeCoordinates(string NumQuarter)
+{
+    if (NumQuarter == "1") return "x > 0, y > 0";
+    if (NumQuarter == "2") return "x < 0, y > 0";
+    if (NumQuarter == "3") return "x < 0, y < 0";
+    if (NumQuarter == "4") return "x > 0, y < 0";
+    return "Ошибка ввода";
+}
+
+string numQuarter = Prompt("Введите номер четверти");
+string RangeCoordinates = rangeCoordinates(numQuarter);
+
+Console.WriteLine(RangeCoordinates);

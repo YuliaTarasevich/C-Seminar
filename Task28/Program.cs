8ea092e620b4bd866288c5b589d016e4b0d50f5c
Yura <yura.tarasevichunt@gmail.com>
2023-10-16 21:19:45 +0300

    Add Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и 
+// выдаёт произведение чисел от 1 до N.
+
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int result = ProductsNumbers(number);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Ошибка ввода, введите ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+
+
+int ProductsNumbers(int numBer)
+{
+    int product = 1;
+    for (int i = 1; i <= numBer; i++)
+    {
+       checked {product = product * i;}
+    }
+    return product;
+}

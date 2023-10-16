455188d14017e2a51e86b17944c0623758c670fa
Yura <yura.tarasevichunt@gmail.com>
2023-10-16 20:35:00 +0300

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, 
+// которая принимает  на вход число и 
+// выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int NumberAmount(int number)
+{
+    if (number < 0)
+    {
+        number = number * (-1);
+    }
+    int amount = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        amount++;
+    }
+    return amount;
+}
+
+int numBer = Prompt("Введите целое число");
+int finish = NumberAmount(numBer);
+Console.WriteLine(finish);

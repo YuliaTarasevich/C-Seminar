e5b31fd67dfed6e905e7c7b34686c2757c0788db
Yura <yura.tarasevichunt@gmail.com>
2023-10-17 18:15:40 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите цикл, который 
+// принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int finish = Convert.ToInt32(value);
+    return finish;
+}
+
+bool IsNumberBOk(int number)
+{
+    if (number <= 0)
+    {
+        Console.WriteLine("Ошибка ввода, введите целое положительное значение степени");
+        return false;
+    }
+    return true;
+}
+
+int Exponentiation(int basDegree, int degree)
+{
+    int num = 1;
+    for (int i = 1; i <=degree; i++)
+    {
+        num = num * basDegree;
+    }
+    return num;
+}
+
+int numberA = Prompt("Введите число A");
+int numberB = Prompt("Введите значение степени B");
+
+if (IsNumberBOk(numberB))
+{
+    int result = Exponentiation(numberA,numberB);
+    Console.WriteLine($"{numberA} в степени {numberB} -> {result}");
+}
+

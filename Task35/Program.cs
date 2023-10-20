b23583fc4ca536be99b0c540c1c9289abb12265d
Yura <yura.tarasevichunt@gmail.com>
2023-10-20 12:40:24 +0300

    add task 35,37

---
+++ Task35/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количесвто элементов массива,
+// значения которых лежат в отрезке [10, 99].
+// [5, 18, 123, 6, 2] -> 1
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountElem(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= 10 && arr[i] < 100)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+int [] array = CreateArrayRndInt (123, 1, 100);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("] ->");
+int result = CountElem (array);
+Console.Write(result);
---
+++ Task37/Program.cs
@@ -0,0 +1,51 @@
+﻿// Найдите произведение пар чисел  в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3  
+// [6 7 3 6] -> 36 21
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] NewArray(int[] arr)
+{
+    int[] newArray = new int[(arr.Length + 1) / 2];
+    for (int i = 0; i < (arr.Length + 1) / 2; i++)
+    {
+        if (i != arr.Length - 1 - i) // добавленная проверка для нечетного количества элементов
+        {
+            newArray[i] = arr[i] * arr[arr.Length - 1 - i];
+        }
+        else
+        {
+            newArray[i] = arr[i]; // добавление последнего элемента
+        }
+    }
+    return newArray;
+}
+
+int[] massive = CreateArrayRndInt(5, 1, 9);
+Console.Write("[");
+PrintArray(massive);
+Console.Write("] --> ");
+int[] newmassive = NewArray(massive);
+PrintArray(newmassive);
+

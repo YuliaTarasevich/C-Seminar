caeada539b1173726ca814570d69de089095692a
Yura <yura.tarasevichunt@gmail.com>
2023-10-22 22:03:50 +0300

    Add Task 41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+void ArrayFromKeyboard(out int[] args)
+{
+
+    Console.WriteLine("Введите число элементов массива");
+    int elementsCount = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[elementsCount];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    args=arr;
+
+    Console.WriteLine("Вывод массива");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int CountPositiveElements(int[] arr)
+{
+    int count = 0;   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+        if (arr[i] > 0)
+        count = count + 1;
+    }
+    return count;
+}
+
+int[] array;
+ArrayFromKeyboard(out array);
+Console.WriteLine();
+int result = CountPositiveElements(array);
+Console.WriteLine($"Количество элементов в введенном массиве больше нуля равно {result}");
---
+++ Task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void IntersectionPoint(double b01, double k01, double b02, double k02)
+{
+    double xCoordinate = (b02 - b01) / (k01 - k02);
+    double yCoordinate = k01 * xCoordinate + b01;
+    Console.WriteLine($"{Math.Round(xCoordinate,2)};{Math.Round(yCoordinate,2)}");
+}
+Console.WriteLine("Введите коэффициент b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+IntersectionPoint(b1, k1, b2, k2);
